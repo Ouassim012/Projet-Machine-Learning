fb354dd116cdfea2f34e32adec9da0a1975b4795
Ouassim012 <12.ouassim@gmail.com>
2024-05-23 19:43:35 +0200

    1th

--- Fast-API/FastApi.py
+++ Fast-API/FastApi.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import io
 import pickle
 from PIL import Image
@@ -76,68 +75,3 @@ async def upload_file(file: UploadFile = File(...)):
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
-=======
-import io
-import pickle
-from PIL import Image
-import cv2
-from fastapi import FastAPI, File, UploadFile
-import numpy as np
-from sklearn.preprocessing import LabelEncoder, StandardScaler
-from sklearn.svm import SVC
-
-app = FastAPI()
-
-# Load the pre-trained SVM model, scaler, and label encoder
-with open('Fast-API/classifier-SVM.pkl', 'rb') as f:
-    model = pickle.load(f)
-
-with open('Fast-API/scaler.pkl', 'rb') as f:
-    scaler = pickle.load(f)
-
-with open('Fast-API/label_encoder.pkl', 'rb') as f:
-    label_encoder = pickle.load(f)
-
-labels = ['Adposhel', 'Agent', 'Allaple', 'Alueron.gen!J', 'Amonetize', 'Androm', 'Autorun', 'BrowseFox', 
-          'C2LOP.gen!g', 'Dialplatform.B', 'Dinwod', 'Elex', 'Expiro', 'Fakerean', 'Fasong', 'HackKMS', 
-          'Hlux', 'Injector', 'InstallCore', 'Lolyda.AA1', 'Lolyda.AA2', 'MultiPlug', 'Neoreklami', 'Neshta', 
-          'Regrun', 'Sality', 'Snarasite', 'Stantinko', 'VBA', 'VBKrypt', 'Vilsel']
-
-# Ensure that label encoder is fitted with the labels
-label_encoder.fit(labels)
-
-def preprocess_image(image, img_height, img_width):
-    image = image.resize((img_height, img_width))  # Resize the image
-    image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)  # Convert PIL Image to NumPy array
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # Convert to grayscale
-    image = image.flatten()  # Flatten image
-    return image
-
-@app.post("/upload/")
-async def upload_file(file: UploadFile = File(...)):
-    # Read the contents of the uploaded file
-    contents = await file.read()
-
-    # Convert the binary data to a PIL Image object
-    image = Image.open(io.BytesIO(contents))
-
-    # Preprocess the image
-    preprocessed_image = preprocess_image(image, 180, 180)
-
-    # Standardize the image
-    preprocessed_image = scaler.transform([preprocessed_image])
-
-    # Predict the class of the image
-    predicted_class_index = model.predict(preprocessed_image)[0]
-
-    # Decode the predicted class index
-    predicted_class = label_encoder.inverse_transform([predicted_class_index])[0]
-
-    print(f"The predicted class of the image is: {predicted_class}")
-
-    return {"predicted_class": predicted_class}
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
->>>>>>> 58f08302c143753dee9954867dea699922988623
